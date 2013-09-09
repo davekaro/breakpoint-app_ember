ff9235ca9ab2926ce5c3d13dc470abc2e71363d5
Dave Kroondyk <davekaro@gmail.com>
2013-09-09 14:01:02 -0400

    better README and seeds file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Breakpoint App (Ember.js edition)
+
+I wanted to learn Ember, so I decided to take Breakpoint App and rewrite it in Ember
+
+## Requirements
+
+* Rails 4 (rails-api) with latest Ruby 2
+* PostgreSQL
+
+## Install
+
+* `git clone`, `bundle install`, `rake db:setup` to get started
+* Two users are created for you
+  * dave@example.com, password
+  * john@example.com, secret
+* Create your user in Rails console
+  * `rails c`
+  * `User.create(first_name: 'Dave', last_name: 'Kroondyk', email: 'dave@example.com', time_zone: 'Eastern Time (US & Canada)', password: 'secret', password_confirmation: 'secret')`
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-User.create(:first_name => 'Dave', :last_name => 'Kroondyk', :phone_number => "555-555-1234", :email => "dave@example.com")
-User.create(:first_name => 'John', :last_name => 'Smith',    :phone_number => "555-555-5678", :email => "john@example.com")
+User.create(first_name: 'Dave', last_name: 'Kroondyk', phone_number: "555-555-1234", email: "dave@example.com", time_zone: "Eastern Time (US & Canada)", password: "password", password_confirmation: "password")
+User.create(first_name: 'John', last_name: 'Smith',    phone_number: "555-555-5678", email: "john@example.com", time_zone: "Eastern Time (US & Canada)", password: "secret",   password_confirmation: "secret")
 
